26a9a40e802de23a3868d2ba2ccd7c1057e3da8a
DuyenAn <antvd22411c@st.uel.edu.vn>
2025-06-08 23:08:11 +0700

    FE Cảm ơn và tiếp tục mua hàng (màn hình 10 trong figma

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.transition:transition:1.4.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
